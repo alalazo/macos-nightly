caeacbd125ae0d5bf56e378f697f0f36a51491d8
Axel Huebl <axel.huebl@plasma.ninja>
2020-04-21 22:58:01 -0700

    Test PR 16222
    https://github.com/spack/spack/pull/16222

--- install_spack.sh
+++ install_spack.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-git clone https://github.com/spack/spack.git
+git clone --branch fix-macOSgccPython https://github.com/ax3l/spack.git
 . spack/share/spack/setup-env.sh
 spack compilers
